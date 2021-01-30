0eb02d2b311081f0097e383357e9f87224062026
AmayaHena <guillaume.le-berre@epitech.eu>
2021-01-30 19:35:22 +0100

    [UP] server request

--- MeguminReactions/HTTPRequests/Reaction.cs
+++ MeguminReactions/HTTPRequests/Reaction.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using MeguminReactions.Data;
+using MeguminReactions.Events;
+using MeguminReactions.Extensions;
+using MeguminReactions.Utils;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +13,17 @@ namespace MeguminReactions.HTTPRequests
 {
     class Reaction
     {
-        public static async Task sendDiscordMessage()
+        public static async Task sendDiscordMessage(HttpMessageEventArgs e)
         {
+            ServerRequest req = new ServerRequest(await e.Request.GetResponseJObject());
 
+            foreach (var guild in Globals.Client.Guilds)
+            {
+                if (guild.GetUser(req.UserId) != null)
+                {
+                    Console.WriteLine("todo");
+                }
+            }
         }
 
     }
---
+++ MeguminReactions/Utils/ServerRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeguminReactions.Utils {
+
+    public class ServerRequest {
+
+        public int MagicNumber { get; private set; }
+        public ulong UserId { get; private set; }
+        public string Message { get; private set; }
+        
+        public ServerRequest(JObject json)
+        {
+            MagicNumber = json.Value<int>("magic_number");
+            UserId = json.Value<ulong>("target");
+            Message = json.Value<string>("message");
+        }
+
+    }
+
+}
