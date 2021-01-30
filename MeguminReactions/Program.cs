bad4fee8567c5bd999f373372bd42e8b8346f627
AmayaHena <guillaume.le-berre@epitech.eu>
2021-01-30 17:27:21 +0100

    [INIT] CS proj

---
+++ MeguminReactions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeguminReactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
